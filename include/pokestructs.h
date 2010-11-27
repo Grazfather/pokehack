efe98a3c63f7eb38ffab96f1e8d2c89a5f76c37c
Grazfather <grazfather@gmail.com>
2010-11-27 00:38:58 -0500

    Fixed a BIG bug in the data structure
    The first unused part was sized one byte, when it should be two bytes. This caused everything after it to be one byte earlier, obviously making the data useless. Changed member x1 to short int from char.

--- include/pokestructs.h
+++ include/pokestructs.h
@@ -1,5 +1,4 @@
-struct pokemon_t
-{
+typedef struct {
 	unsigned int personality;
 	unsigned int otid;
 	unsigned char name[10];
@@ -8,7 +7,7 @@ struct pokemon_t
 	unsigned char otname[7];
 	unsigned char mark;
 	unsigned short int checksum;
-	unsigned char x1; 				// unused
+	unsigned short int x1; 				// unused
 	unsigned char data[48];
 	unsigned int status;
 	unsigned char level;
@@ -20,20 +19,18 @@ struct pokemon_t
 	unsigned short speed;
 	unsigned short spatk;
 	unsigned short spdef;
-};
+} pokemon_t;
 
-struct pokemon_growth_t
-{
+typedef struct{
 	unsigned short int species;
 	unsigned short int held;
 	unsigned int xp;
 	unsigned char ppbonuses;
 	unsigned char happiness;
 	unsigned char x; 				// unused
-};
+} pokemon_growth_t ;
 
-struct pokemon_attacks_t
-{
+typedef struct {
 	unsigned short int atk1;
 	unsigned short int atk2;
 	unsigned short int atk3;
@@ -42,9 +39,9 @@ struct pokemon_attacks_t
 	unsigned char pp2;
 	unsigned char pp3;
 	unsigned char pp4;
-};
+} pokemon_attacks_t;
 
-struct pokemon_effort_t
+typedef struct
 {
 	unsigned char hp;
 	unsigned char attack;
@@ -58,14 +55,50 @@ struct pokemon_effort_t
 	unsigned char smartness;
 	unsigned char toughness;
 	unsigned char feel;
-};
+} pokemon_effort_t;
 
-struct pokemon_misc_t
-{
+typedef struct {
 	unsigned char pokerus;
 	unsigned char location;
 	signed char level;
 	unsigned char pokeball; 		// As well as trainer gender?
 	unsigned int IVs; 				// TODO: Split into groups of 5 bytes + 2 for egg
 	unsigned int ribbons;
-};
+} pokemon_misc_t;
+
+/* Order:
+	00. GAEM	 06. AGEM	 12. EGAM	 18. MGAE
+	01. GAME	 07. AGME	 13. EGMA	 19. MGEA
+	02. GEAM	 08. AEGM	 14. EAGM	 20. MAGE
+	03. GEMA	 09. AEMG	 15. EAMG	 21. MAEG
+	04. GMAE	 10. AMGE	 16. EMGA	 22. MEGA
+	05. GMEA	 11. AMEG	 17. EMAG	 23. MEAG
+*/
+
+// Where in data each struct is, based on AEGM order
+static const int DataOrderTable[24][3] = { \
+/*          A  E  G  M */ \
+/* GAEM */ {1, 2, 0, 3}, \
+/* GAME */ {1, 3, 0, 2}, \
+/* GEAM */ {2, 1, 0, 3}, \
+/* GEMA */ {3, 1, 0, 2}, \
+/* GMAE */ {2, 3, 0, 1}, \
+/* GMEA */ {3, 2, 0, 1}, \
+/* AGEM */ {0, 2, 1, 3}, \
+/* AGME */ {0, 3, 1, 2}, \
+/* AEGM */ {0, 1, 2, 3}, \
+/* AEMG */ {0, 1, 3, 2}, \
+/* AMGE */ {0, 3, 2, 1}, \
+/* AMEG */ {0, 2, 3, 1}, \
+/* EGAM */ {2, 0, 1, 3}, \
+/* EGMA */ {3, 0, 1, 2}, \
+/* EAGM */ {1, 0, 2, 3}, \
+/* EAMG */ {1, 0, 3, 2}, \
+/* EMGA */ {3, 0, 2, 1}, \
+/* EMAG */ {2, 0, 3, 1}, \
+/* MGAE */ {2, 3, 1, 0}, \
+/* MGEA */ {3, 2, 1, 0}, \
+/* MAGE */ {1, 3, 2, 0}, \
+/* MAEG */ {1, 2, 3, 0}, \
+/* MEGA */ {3, 1, 2, 0}, \
+/* MEAG */ {2, 1, 3, 0} };
