691be4080def04bbb417096cccce4cd51ae44874
Grazfather <grazfather@gmail.com>
2013-04-04 22:19:19 -0700

    Only print errors to stderr
    Print normal text to stdout instead of stderr.

--- src/pkg3.c
+++ src/pkg3.c
@@ -77,7 +77,7 @@ int parse_pokemon(char* buf, int offset, void** pokemon, pokemon_attacks_t** pa,
 		pg[i] = (pokemon_growth_t *)(((box_pokemon_t*)pokemon[i])->data + DataOrderTable[o][2] * sizeof(pokemon_growth_t));
 		pm[i] = (pokemon_misc_t *)(((box_pokemon_t*)pokemon[i])->data + DataOrderTable[o][3] * sizeof(pokemon_misc_t));
 		if (pg[i]->species) {
-			fprintf(stderr, "\nPokemon %d\n", i);
+			fprintf(stdout, "\nPokemon %d\n", i);
 			print_pokemon(pokemon[i]);
 		}
 	}
@@ -102,10 +102,10 @@ void print_pokemon(box_pokemon_t* pokemon)
 	pm = (pokemon_misc_t *)(pokemon->data + DataOrderTable[o][3] * sizeof(pokemon_misc_t));
 
 	totalIVs = pm->hpiv + pm->atkiv + pm->defiv + pm->spatkiv + pm->spdefiv + pm->spdiv;
-	fprintf(stderr, "Species %d: %s, held %d: %s, experience: %d, ppb: %d, friendship: %d\n", pg->species, pokemon_species[pg->species], pg->held, items[pg->held], pg->xp, pg->ppbonuses, pg->happiness);
-	fprintf(stderr, "Attacks: 1:%d, 2:%d, 3:%d, 4:%d, pp1:%d, pp2:%d, pp3:%d, pp4:%d\n", pa->atk1, pa->atk2, pa->atk3, pa->atk4, pa->pp1, pa->pp2, pa->pp3, pa->pp4 );
-	fprintf(stderr, "IVs: hp:%d, atk:%d, def:%d, spatk:%d, spdef:%d, spd:%d, Total:%d \n", pm->hpiv, pm->atkiv, pm->defiv, pm->spatkiv, pm->spdefiv, pm->spdiv, totalIVs );
-	fprintf(stderr, "EVs: hp:%d, atk:%d, def:%d, spatk:%d, spdef:%d, spd:%d\n", pe->hp, pe->attack, pe->defense, pe->spatk, pe->spdef, pe->speed );
+	fprintf(stdout, "Species %d: %s, held %d: %s, experience: %d, ppb: %d, friendship: %d\n", pg->species, pokemon_species[pg->species], pg->held, items[pg->held], pg->xp, pg->ppbonuses, pg->happiness);
+	fprintf(stdout, "Attacks: 1:%d, 2:%d, 3:%d, 4:%d, pp1:%d, pp2:%d, pp3:%d, pp4:%d\n", pa->atk1, pa->atk2, pa->atk3, pa->atk4, pa->pp1, pa->pp2, pa->pp3, pa->pp4 );
+	fprintf(stdout, "IVs: hp:%d, atk:%d, def:%d, spatk:%d, spdef:%d, spd:%d, Total:%d \n", pm->hpiv, pm->atkiv, pm->defiv, pm->spatkiv, pm->spdefiv, pm->spdiv, totalIVs );
+	fprintf(stdout, "EVs: hp:%d, atk:%d, def:%d, spatk:%d, spdef:%d, spd:%d\n", pe->hp, pe->attack, pe->defense, pe->spatk, pe->spdef, pe->speed );
 }
 
 int main(int argc, char * argv[])
@@ -163,7 +163,7 @@ int main(int argc, char * argv[])
 			fprintf(stderr, "Could not save changes!\n");
 			return -1;
 		} else {
-			fprintf(stderr, "Saved!\n");
+			fprintf(stdout, "Saved!\n");
 		}
 	} else {
 		free(unpackeddata);
